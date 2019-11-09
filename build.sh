bfdb310838b1649b12e6c94314253d2063580c4f
rafi@guengel.ch <rafi@guengel.ch>
2019-11-09 10:47:16 +0100

    Beef up build script and drop go module

--- build.sh
+++ build.sh
@@ -2,15 +2,45 @@
 
 set -eu
 
+PROTOC_VERSION=3.10.1
+
+install_protoc() {
+    if [ -d protoc ]
+    then
+	return 0
+    fi
+
+    mkdir protoc
+
+    curl -Lo /tmp/protoc.zip "https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VERSION}/protoc-${PROTOC_VERSION}-linux-x86_64.zip"
+    unzip /tmp/protoc.zip -d protoc
+}
+
+install_protoc_gen_go() {
+    go get -u github.com/golang/protobuf/protoc-gen-go
+}
+
+
 build_go() {
-    protoc -I schema --go_out=plugins=grpc:. nmap.proto
+    protoc --go_out=plugins=grpc:. src/github.com/rafaelostertag/nmapservice/nmap.proto
 }
 
 build_java() {
     mkdir -p src/main/proto
-    cp -f schema/*.proto src/main/proto
+    cp -f src/github.com/rafaelostertag/nmapservice/*.proto src/main/proto
     mvn compile
 }
 
+install_protoc
+
+GOPATH="$PWD/go"
+export GOPATH
+
+install_protoc_gen_go
+
+unset GOPATH
+
+PATH="$PWD/protoc/bin:$PWD/go/bin:$PATH"
+export PATH
 build_go
 build_java
